e6ee9c3649c37c9133c9003ade8c8550ca11669b
ahmad alfarel <84859336+ocdsquad@users.noreply.github.com>
2023-06-02 17:22:52 +0700

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $prods = Product::get();
+        if(request()->segment(1) == 'api') return response()->json([
+            "error" => false,
+            "list" => $prods,
+        ]);
+        return view('product.index', ['list' => $prods]);
+    }
+
+    public function create()
+    {
+        return view('product.form', [
+            'title' => 'Tambah',
+            'method' => 'POST',
+            'action' => 'product'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'price' => 'required|integer|min:1000000'
+        ]);
+
+        $prod = new Product;
+        $prod->name = $request->name;
+        $prod->price = $request->price;
+        $prod->save();
+        if(request()->segment(1) == 'api') return response()->json([
+            "error" => false,
+            "message" => 'Tambah berhasil'
+        ]);
+        return redirect('/product')->with('msg', 'Tambah berhasil');
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function edit($id)
+    {
+        return view('product.form', [
+            'title' => 'Edit',
+            'method' => 'PUT',
+            'action' => "product/$id",
+            'data' => Product::find($id)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'price' => 'required|integer|min:1000000'
+        ]);
+
+        $prod = Product::find($id);
+        $prod->name = $request->name;
+        $prod->price = $request->price;
+        $prod->save();
+        return redirect('/product')->with('msg', 'Edit berhasil');
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        // atau
+        /* $prod = Product::find($id);
+        $prod->delete(); */
+        return redirect('/product')->with('msg', 'Hapus berhasil');
+    }
+
+}
